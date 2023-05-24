e8d4ecc636bb490d14d66a90e0380a0e5be9947a
aathshamehar <aathsha@gmail.com>
2023-05-24 11:56:23 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,74 @@
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class KeralaStateComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterValue: '',
+    };
+  }
+
+  handleFilterChange = (event) => {
+    this.setState({ filterValue: event.target.value });
+  };
+
+  render() {
+    const cities = [
+      {  id: 1, name: 'Kasarkod', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Kasrkod-beach-park-Honnavar.jpg/1200px-Kasrkod-beach-park-Honnavar.jpg' },
+      {  id: 2, name: 'Kannur', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Muzhappilangad_Drive-in_Beach_2.jpg/1024px-Muzhappilangad_Drive-in_Beach_2.jpg' },
+      {  id: 3, name: 'Calicut', image: 'https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcRwnTyfBU1t9qnloBvM4AYe4eVXKmfztoyzesrlbOVQdjHGCoRIO7LU3jJJOPVpsUwEetTV0HiQMx7GYoA1eumymdp41Q' },
+      {  id: 4, name: 'Wayanad', image: 'https://www.wayanad.com/files/menuboxes/108009770.jpg' },
+      {  id: 5, name: 'Malappuram', image: 'https://cdn.s3waas.gov.in/s382aa4b0af34c2313a562076992e50aa3/uploads/2018/04/2018041752-2.jpg' },
+      {  id: 6, name: 'Palakkad', image: 'https://www.tusktravel.com/blog/wp-content/uploads/2022/12/Malampuzha-Park-Palakkad-Kerala.jpg' },
+      {  id: 7, name: 'Thrissur', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvsc376BoB-Tnb7m10osDSXsV15oXL98UD4zMduln4AQ&usqp=CAU&ec=48665701' },
+      {  id: 8, name: 'Kochi', image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/de/f0/eb/backwater-tourism.jpg?w=700&h=-1&s=1' },
+      {  id: 9, name: 'Alappuzha', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Alappuzha_Boat_Beauty_W.jpg/1024px-Alappuzha_Boat_Beauty_W.jpg' },
+      {  id: 10, name: 'Idukki', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Munnar_Top_station.jpg/1024px-Munnar_Top_station.jpg' },
+      {  id: 11, name: 'Kollam', image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/13_Arch_bridge_Kazhuthurutty_Kerala.jpg' },
+      {  id: 12, name: 'Kottayam', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vembanad_Lake_at_Kumarakom.jpg/1280px-Vembanad_Lake_at_Kumarakom.jpg' },
+      {  id: 13, name: 'Pathanamthitta', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/33kVsubstationperunad.jpeg/1280px-33kVsubstationperunad.jpeg' },
+      {  id: 14, name: 'Thiruvananthapuram', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Sanghumugham_Beach_in_TVM.jpg/1024px-Sanghumugham_Beach_in_TVM.jpg' }
+    ];
+
+    const filteredCities = cities.filter((city) =>
+      city.name.toLowerCase().includes(this.state.filterValue.toLowerCase()) ||
+      city.id.toString().includes(this.state.filterValue)
+    );
+
+    return (
+      <div>
+        <h1 className="text-center">KERALA STATE</h1>
+
+        <div className="text-center">
+          <input
+            type="text"
+            id="filterInput"
+            placeholder="Filter by city or ID"
+            className="form-control"
+            value={this.state.filterValue}
+            onChange={this.handleFilterChange}
+          />
+        </div>
+
+        <Container className="text-center">
+          <Row>
+            {filteredCities.map((city, index) => (
+              <Col key={index} sm={12} md={6} lg={4} xl={3}>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={city.image} alt={city.name} />
+                  <Card.Body>
+                    <Card.Title>{city.name}</Card.Title>
+                    <Card.Text>ID: {city.id}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default KeralaStateComponent;
